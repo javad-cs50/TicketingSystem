d80010e169cb685beeefb3fb28a1b2241a8c40f3
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-03 10:09:43 +0330

    chore(Infrastructure): scaffold infrastructure project structure

---
+++ TicketingSystem.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Infrastructure.Persistence
+{
+    internal class ApplicationDbContext:DbContext
+    {
+
+    }
+}
