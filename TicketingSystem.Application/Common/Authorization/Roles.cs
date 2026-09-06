1592d32d8ddfd9b6a9b65657316e99f0e55f18e7
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-06 14:45:42 +0330

    chore(application) : add the user roles.

---
+++ TicketingSystem.Application/Common/Authorization/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketingSystem.Application.Common.Authorization;
+
+public static class Roles
+{
+    public const string PlatformAdmin = "PlatformAdmin";
+    public const string TenantAdmin = "TenantAdmin";
+    public const string Agent = "Agent";
+    public const string Customer = "Customer";
+}
