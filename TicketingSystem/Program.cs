8e8d5edd4e5486c24a91a94eeb887971f6c6af7e
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-08 09:44:02 +0330

    chore(api) : adding infrastructure dependency injection and also seed the roles

--- TicketingSystem/Program.cs
+++ TicketingSystem/Program.cs
@@ -1,13 +1,25 @@
+
+using Microsoft.AspNetCore.Identity;
+using TicketingSystem.Infrastructure;
+using TicketingSystem.Infrastructure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+// Add Clean Architecture Layers
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var roleManger = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+    await IdentitySeeder.SeedAsync(roleManger);
+}
+;
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
