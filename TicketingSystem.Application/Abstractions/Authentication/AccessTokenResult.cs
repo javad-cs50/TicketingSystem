fd373b4734490a087d8d04614799463a82491720
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-08 10:53:38 +0330

    refactor(application) : add a record(dto) for output of TokenService.GenerateAccessToken.

---
+++ TicketingSystem.Application/Abstractions/Authentication/AccessTokenResult.cs
@@ -0,0 +1,2 @@
+﻿namespace TicketingSystem.Application.Abstractions.Authentication;
+public sealed record AccessTokenResult(string Token, DateTime ExpiresAt);
--- TicketingSystem.Application/Abstractions/Authentication/ITokenService.cs
+++ TicketingSystem.Application/Abstractions/Authentication/ITokenService.cs
@@ -2,7 +2,7 @@
 
 public interface ITokenService
 {
-    string GenerateAccessToken(Guid userId, Guid? tenantId, IEnumerable<string> roles);
+    AccessTokenResult GenerateAccessToken(Guid userId, Guid? tenantId, IEnumerable<string> roles);
     string GenerateRefreshToken();
     string HashRefreshToken(string token);
 }
