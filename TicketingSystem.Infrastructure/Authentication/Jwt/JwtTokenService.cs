0b0152dc4823245168bf5947d43542bd74f6f080
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-09 10:03:25 +0330

    chore(infrastructure) : implementing GenerateAccessToken in JwtTokenService class.

--- TicketingSystem.Infrastructure/Authentication/Jwt/JwtTokenService.cs
+++ TicketingSystem.Infrastructure/Authentication/Jwt/JwtTokenService.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using TicketingSystem.Application.Abstractions.Authentication;
 
 namespace TicketingSystem.Infrastructure.Authentication.Jwt;
@@ -9,7 +13,33 @@ public sealed class JwtTokenService(IOptions<JwtOptions> options) : ITokenServic
 
     public AccessTokenResult GenerateAccessToken(Guid userId, Guid? tenantId, IEnumerable<string> roles)
     {
-        throw new NotImplementedException();
+        var claims = new List<Claim>
+    {
+        new(ClaimTypes.NameIdentifier, userId.ToString())
+    };
+
+        if (tenantId.HasValue)
+        {
+            claims.Add(new Claim("tenant_id", tenantId.Value.ToString()));
+        }
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var expiresAt = DateTime.UtcNow.AddMinutes(_options.AccessTokenExpirationMinutes);
+
+        var token = new JwtSecurityToken
+            (issuer: _options.Issuer, audience: _options.Audience, claims: claims, expires: expiresAt, signingCredentials: credentials);
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return new AccessTokenResult(tokenString, expiresAt);
     }
 
     public string GenerateRefreshToken()
