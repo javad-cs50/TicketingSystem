cbf349bee7b7e4e246c4d7ab50e457d4b80f099b
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-10 09:59:26 +0330

    feat(infrastructure) : add refresh token repository

---
+++ TicketingSystem.Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Application.Abstractions.Persistence;
+using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Infrastructure.Persistence.Repositories;
+
+public sealed class RefreshTokenRepository(
+ApplicationDbContext context)
+: IRefreshTokenRepository
+{
+    public async Task<RefreshToken?> GetByHashAsync(
+        string tokenHash,
+        CancellationToken cancellationToken = default)
+    {
+        return await context.RefreshTokens
+            .SingleOrDefaultAsync(
+                x => x.TokenHash == tokenHash,
+                cancellationToken);
+    }
+
+    public async Task AddAsync(
+        RefreshToken refreshToken,
+        CancellationToken cancellationToken = default)
+    {
+        await context.RefreshTokens.AddAsync(
+            refreshToken,
+            cancellationToken);
+    }
+}
