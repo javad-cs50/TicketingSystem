410811d2e30f2c95b9fea5410a1494ec21cf1262
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-10 09:47:56 +0330

    feat(infrastructure) : add refresh token configuration

---
+++ TicketingSystem.Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Infrastructure.Persistence.Configurations;
+
+public sealed class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.TokenHash)
+            .IsRequired()
+            .HasMaxLength(64);
+
+        builder.HasIndex(x => x.TokenHash)
+            .IsUnique();
+
+        builder.Property(x => x.ExpiresAt)
+            .IsRequired();
+
+        builder.Property(x => x.RevokedAt);
+
+        builder.HasIndex(x => new
+        {
+            x.UserId,
+            x.ExpiresAt
+        });
+    }
+}
