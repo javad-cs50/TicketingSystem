1ad843b62e1cfb4750892f7b4ab5cb93e8cf6b3a
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-10 09:42:51 +0330

    feat(domain) : add refresh token entity

---
+++ TicketingSystem.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,30 @@
+﻿using TicketingSystem.Domain.Common;
+
+namespace TicketingSystem.Domain.Entities;
+
+public sealed class RefreshToken : BaseEntity
+{
+    public Guid UserId { get; private set; }
+    public string TokenHash { get; private set; } = null!;
+    public DateTime ExpiresAt { get; private set; }
+    public DateTime? RevokedAt { get; private set; }
+    public Guid? ReplacedByTokenId { get; private set; }
+    public bool IsRevoked => RevokedAt.HasValue;
+    public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+    //to prevent creating invalid object
+    private RefreshToken() { }
+    public RefreshToken(Guid userId, string tokenHash, DateTime expiresAt)
+    {
+        UserId = userId;
+        TokenHash = tokenHash;
+        ExpiresAt = expiresAt;
+    }
+
+    public void Revoke()
+    {
+        if (IsRevoked)
+            return;
+
+        RevokedAt = DateTime.UtcNow;
+    }
+}
