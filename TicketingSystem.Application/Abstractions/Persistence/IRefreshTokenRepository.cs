a3bda603e05759783a9513fa1333e8aa02e443b4
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-10 09:56:59 +0330

    feat(application) : add refresh token repository interface

---
+++ TicketingSystem.Application/Abstractions/Persistence/IRefreshTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Application.Abstractions.Persistence;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByHashAsync(string tokenHash, CancellationToken cancellationToken = default);
+
+    Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken = default);
+}
