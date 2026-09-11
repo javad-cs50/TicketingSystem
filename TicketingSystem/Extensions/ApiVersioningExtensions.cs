ec3cd987c9651adee0752435730fac73dcb5ccc7
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-11 12:14:28 +0330

    refactor(api): extract API versioning configuration

---
+++ TicketingSystem/Extensions/ApiVersioningExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Asp.Versioning;
+
+namespace TicketingSystem.Api.Extensions;
+public static class ApiVersioningExtensions
+{
+    public static IServiceCollection AddApiVersioningConfiguration(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = false;
+            options.ReportApiVersions = true;
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+        }).AddApiExplorer(options =>
+        {
+            options.GroupNameFormat="'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+        return services;
+    }
+}
