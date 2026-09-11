4c172e544c5f9ee3c79f39e24de1e564af691248
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-11 10:30:54 +0330

    feat(application) : add refresh token result for accessing expire date without refrencing jwtOption inside the application layer

---
+++ TicketingSystem.Application/Abstractions/Authentication/RefreshTokenResult.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Application.Abstractions.Authentication;
+
+public sealed record RefreshTokenResult(string Token, DateTime ExpireAt);
+
