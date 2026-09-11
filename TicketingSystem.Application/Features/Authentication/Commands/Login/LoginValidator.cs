7c4ab166fd5915095ed341ed08e49ff98094a24a
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-11 10:50:06 +0330

    feat(auth): add login validator

---
+++ TicketingSystem.Application/Features/Authentication/Commands/Login/LoginValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace TicketingSystem.Application.Features.Authentication.Commands.Login;
+
+public sealed class LoginValidator :AbstractValidator<LoginCommand>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
