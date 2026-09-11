c312f50f8c886d7b978592a3ee116f1908501d78
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-11 10:00:59 +0330

    feat(infrastructure) : add unit of work

---
+++ TicketingSystem.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using TicketingSystem.Application.Abstractions.Persistence;
+
+namespace TicketingSystem.Infrastructure.Persistence;
+
+public sealed class UnitOfWork(ApplicationDbContext dbContext) : IUnitOfWork
+{
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => dbContext.SaveChangesAsync(cancellationToken);
+}
