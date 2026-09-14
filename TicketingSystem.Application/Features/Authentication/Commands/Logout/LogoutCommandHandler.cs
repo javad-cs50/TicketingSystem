bc5afb3fa008b889b38dd4467fcaf0f67f7e79b7
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-14 19:22:24 +0330

    feat(auth): add logout

---
+++ TicketingSystem.Application/Features/Authentication/Commands/Logout/LogoutCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using TicketingSystem.Application.Abstractions.Authentication;
+using TicketingSystem.Application.Abstractions.Persistence;
+
+namespace TicketingSystem.Application.Features.Authentication.Commands.Logout;
+
+public sealed record LogoutCommandHandler
+    (IRefreshTokenRepository RefreshTokenRepository ,
+    IUnitOfWork UnitOfWork ,
+    ITokenService TokenService
+    ) 
+    : IRequestHandler<LogoutCommand>
+{
+    public async Task Handle(LogoutCommand request, CancellationToken cancellationToken)
+    {
+        var hashRefreshToken = TokenService.HashRefreshToken(request.RefreshToken);
+        var refreshTokenFromDb = await RefreshTokenRepository.GetByHashAsync(hashRefreshToken,cancellationToken);
+        if (refreshTokenFromDb ==null || refreshTokenFromDb.IsRevoked)
+            return;
+        refreshTokenFromDb.Revoke();
+
+        await UnitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
