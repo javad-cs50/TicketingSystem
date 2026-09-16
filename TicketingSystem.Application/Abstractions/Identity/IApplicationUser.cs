b57e3dd59187f5f6a95c2701f594a62274ba8a1b
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-16 16:23:11 +0330

    refactor(application): add IApplicationUser that can intract with application user in infrastructure

---
+++ TicketingSystem.Application/Abstractions/Identity/IApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿namespace TicketingSystem.Application.Abstractions.Identity;
+
+public interface IApplicationUser
+{
+    Guid Id { get; }
+    string? UserName { get; }
+    string? Email { get; }
+    Guid? TenantId { get; }
+    bool IsActive { get; }
+    
+    void SetTenant(Guid tenantId);
+    void Activate();
+    void Deactivate();
+}
--- TicketingSystem.Infrastructure/Identity/ApplicationUser.cs
+++ TicketingSystem.Infrastructure/Identity/ApplicationUser.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using TicketingSystem.Application.Abstractions.Identity;
 
 namespace TicketingSystem.Infrastructure.Identity;
 
-public sealed class ApplicationUser:IdentityUser<Guid>
+public sealed class ApplicationUser:IdentityUser<Guid>,IApplicationUser
 {
-    public Guid? TenantId { get; set; }
-    public bool IsActive { get; set; }
+    public Guid? TenantId { get; private set; }
+    public bool IsActive { get; private set; } = true;
+
+    public void Activate()=> IsActive = true;
+    public void Deactivate() => IsActive = false;
+    public void SetTenant(Guid tenantId)=>TenantId = tenantId;
 }
