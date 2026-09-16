73cde3fd3826c9b693c7a5ebdb08665f31e6f643
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-16 16:27:41 +0330

    feat(application): add getByIdAsync to IUserRepository

--- TicketingSystem.Application/Abstractions/Persistence/IUserRepository.cs
+++ TicketingSystem.Application/Abstractions/Persistence/IUserRepository.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using TicketingSystem.Application.Abstractions.Identity;
 
 namespace TicketingSystem.Application.Abstractions.Persistence;
 
 public interface IUserRepository
 {
+    Task<IApplicationUser?> GetByIdAsync(Guid userId,Guid TenantId,CancellationToken cancellationToken);
+    Task<IApplicationUser?> GetByIdNoTrackAsync(Guid userId,Guid TenantId,CancellationToken cancellationToken);
+    Task<IReadOnlyList<IApplicationUser>?> GetListAsync(Guid TenantId, CancellationToken cancellationToken);
+
+
 }
