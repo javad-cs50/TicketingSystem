cfc4e0f59a52336026af5616f095c8a29038842f
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-16 08:43:40 +0330

    feat(application): add tenant validation for update

---
+++ TicketingSystem.Application/Features/Tenants/Commands/Update/CreateTenantValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace TicketingSystem.Application.Features.Tenants.Commands.Update;
+
+public sealed class UpdateTenantValidator:AbstractValidator<UpdateTenantCommand>
+{
+    public UpdateTenantValidator()
+    {
+        RuleFor(t => t.TenantId)
+            .NotEmpty();
+
+        RuleFor(t => t.Name)
+            .NotEmpty()
+            .MaximumLength(150);
+
+        RuleFor(t=>t.Slug)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
