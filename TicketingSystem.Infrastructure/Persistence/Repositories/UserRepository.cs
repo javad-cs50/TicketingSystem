f22d6e13e189195ab80508273462ba4c397f3cdb
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-16 16:24:01 +0330

    feat(infrastructure): add user repository

---
+++ TicketingSystem.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Application.Abstractions.Identity;
+using TicketingSystem.Application.Abstractions.Persistence;
+
+namespace TicketingSystem.Infrastructure.Persistence.Repositories
+{
+    internal class UserRepository(ApplicationDbContext applicationDbContext) : IUserRepository
+    {
+        public async Task<IApplicationUser?> GetByIdAsync(Guid userId, Guid tenantId, CancellationToken cancellationToken)
+            => await applicationDbContext.Users
+                .Where(u => u.TenantId == tenantId)
+                .FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+        public async Task<IApplicationUser?> GetByIdNoTrackAsync(Guid userId, Guid tenantId, CancellationToken cancellationToken)
+            => await applicationDbContext.Users
+                .AsNoTracking()
+                .Where(u => u.TenantId == tenantId)
+                .FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+        public async Task<IReadOnlyList<IApplicationUser>?> GetListAsync(Guid tenantId, CancellationToken cancellationToken)
+            => await applicationDbContext.Users
+                .AsNoTracking()
+                .Where(u => u.TenantId == tenantId)
+                .Cast<IApplicationUser>()
+                .ToListAsync(cancellationToken);
+
+    }
+}
