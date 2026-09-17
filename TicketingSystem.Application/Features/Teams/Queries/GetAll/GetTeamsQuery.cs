8fe5422976e2b26eee93e3445f81a6e71aeee312
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-17 11:19:53 +0330

    feat(application): add get users feature

---
+++ TicketingSystem.Application/Features/Teams/Queries/GetAll/GetTeamsQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TicketingSystem.Application.Features.Teams.DTOs;
+
+namespace TicketingSystem.Application.Features.Teams.Queries.GetAll;
+
+public sealed record GetTeamsQuery : IRequest<IReadOnlyList<TeamResponse>?>;
---
+++ TicketingSystem.Application/Features/Teams/Queries/GetAll/GetTeamsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using TicketingSystem.Application.Abstractions.Identity;
+using TicketingSystem.Application.Abstractions.Persistence;
+using TicketingSystem.Application.Features.Teams.DTOs;
+
+namespace TicketingSystem.Application.Features.Teams.Queries.GetAll;
+
+public sealed class GetTeamsQueryHandler
+    (ITeamRepository teamRepository, ICurrentUserService currentUserService)
+    : IRequestHandler<GetTeamsQuery, IReadOnlyList<TeamResponse>?>
+{
+    public async Task<IReadOnlyList<TeamResponse>?> Handle(GetTeamsQuery request, CancellationToken cancellationToken)
+    {
+        var tenantId = currentUserService.TenantId ?? throw new UnauthorizedAccessException("Tenant context is missing.");
+
+        var teams = await teamRepository.GetListAsync(tenantId, cancellationToken);
+        if (teams == null)
+            return [];
+
+        return teams.Select(team => new TeamResponse(team.Id, team.TenantId, team.Name)).ToList();
+    }
+}
