42033646ac27bf27280ecca6c546390a98fc0ae5
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-17 11:20:21 +0330

    feat(application): add get user by id feature

---
+++ TicketingSystem.Application/Features/Teams/Queries/GetById/GetTeamByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TicketingSystem.Application.Features.Teams.DTOs;
+
+namespace TicketingSystem.Application.Features.Teams.Queries.GetById;
+
+public sealed record GetTeamByIdQuery(Guid TeamId) : IRequest<TeamResponse?>;
---
+++ TicketingSystem.Application/Features/Teams/Queries/GetById/GetTeamByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using TicketingSystem.Application.Abstractions.Identity;
+using TicketingSystem.Application.Abstractions.Persistence;
+using TicketingSystem.Application.Features.Teams.DTOs;
+
+namespace TicketingSystem.Application.Features.Teams.Queries.GetById;
+
+public sealed class GetTeamByIdQueryHandler
+    (ITeamRepository teamRepository, ICurrentUserService currentUserService)
+    : IRequestHandler<GetTeamByIdQuery, TeamResponse?>
+{
+    public async Task<TeamResponse?> Handle(GetTeamByIdQuery request, CancellationToken cancellationToken)
+    {
+        var tenantId = currentUserService.TenantId ?? throw new UnauthorizedAccessException("Tenant context is missing.");
+
+        var team = await teamRepository.GetByIdNoTrackAsync(request.TeamId, cancellationToken);
+
+        if (team is null)
+            return null;
+
+        return new TeamResponse(team.Id, team.TenantId, team.Name);
+    }
+}
