8efe1c4de5b8b261d7b36b838415008cd823ea6a
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-17 10:03:57 +0330

    feat(application): complete update user feature

--- TicketingSystem.Application/Features/Users/Commands/Update/UpdateUserCommandHandler.cs
+++ TicketingSystem.Application/Features/Users/Commands/Update/UpdateUserCommandHandler.cs
@@ -1,12 +1,22 @@
 ﻿using MediatR;
+using TicketingSystem.Application.Abstractions.Identity;
 using TicketingSystem.Application.Abstractions.Persistence;
 
 namespace TicketingSystem.Application.Features.Users.Commands.Update;
 
-public sealed class UpdateUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : IRequestHandler<UpdateUserCommand>
+public sealed class UpdateUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork,ICurrentUserService currentUserService) : IRequestHandler<UpdateUserCommand>
 {
-    public Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    public async Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
-        
+        var tenantId = currentUserService.TenantId;
+        if (tenantId is null)
+            throw new UnauthorizedAccessException("Tenant context is missing.");
+
+        var userFromDb = await userRepository.GetByIdAsync(request.Id, tenantId.Value, cancellationToken);
+        if (userFromDb is null)
+            throw new KeyNotFoundException($"the id : {request.Id} not founded.");
+
+        userFromDb.Update(request.UserName, request.Email);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
     }
 }
