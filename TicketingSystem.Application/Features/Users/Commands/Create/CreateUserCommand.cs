4a77503aacf010ae926b6fc8e31dfc4b4b5085d7
javad-cs50 <172836432+javad-cs50@users.noreply.github.com>
2026-09-17 09:34:46 +0330

    refactor(application): remove obsolete file

--- TicketingSystem.Application/Features/Users/Commands/Create/CreateUserCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace TicketingSystem.Application.Features.Users.Commands.Create;
-
-public sealed record CreateUserCommand(string UserName , string Email):IRequest<Guid>;
--- TicketingSystem.Application/Features/Users/Commands/Delete/DeleteUserCommandHandler.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace TicketingSystem.Application.Features.Users.Commands.Delete;
-
-public sealed class DeleteUserCommandHandler
-{
-}
